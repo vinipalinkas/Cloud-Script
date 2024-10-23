91ef2de4c5580119f03f42ae95be5782aae1caa3
⸸ Palinkas <107942532+vinipalinkas@users.noreply.github.com>
2024-10-23 16:47:00 -0300

    Update user-desabilitados-grupos.ps1

--- user-desabilitados-grupos.ps1
+++ user-desabilitados-grupos.ps1
@@ -1,27 +1,17 @@
 Connect-AzAccount -Identity
 
 $groups = Get-AzADGroup
-$users = Get-AzADUser -Filter "accountEnabled eq false"
+$disabledUsers = Get-AzADUser -Filter "accountEnabled eq false"
 
 $excludedGroups = @("Domain Users", "Licenca_Office_365_Desabilitados", "Todos os Usuários (Desativados e Ativos)")
 $excludedGroupIds = $groups | Where-Object { $excludedGroups -contains $_.DisplayName } | Select-Object -ExpandProperty Id
 
-$groupMembers = @{}
-
-foreach ($group in $groups) {
-    if ($excludedGroupIds -notcontains $group.Id) {
-        $members = Get-AzADGroupMember -GroupObjectId $group.Id
+foreach ($user in $disabledUsers) {
+    foreach ($group in $groups) {
+        $isMember = Get-AzADGroupMember -GroupObjectId $group.Id | Where-Object { $_.Id -eq $user.Id }
         
-        $groupMembers[$group.Id] = $members
-    }
-}
-
-foreach ($user in $users) {
-    foreach ($groupId in $groupMembers.Keys) {
-        $isMember = $groupMembers[$groupId] | Where-Object { $_.Id -eq $user.Id }
-        
-        if ($isMember) {
-            Remove-AzADGroupMember -GroupObjectId $groupId -MemberObjectId $user.Id
+        if ($isMember -and ($excludedGroupIds -notcontains $group.Id)) {
+            Remove-AzADGroupMember -GroupObjectId $group.Id -MemberObjectId $user.Id
         }
     }
 }
